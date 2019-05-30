9c6abb44c394a2a75b1361fd5a4bee59884a0a04
ManuCosmo <emmanuel.andre@cosmosoftware.io>
2019-05-30 17:08:37 +0800

    StepPhase improvements

--- src/main/java/io/cosmosoftware/kite/steps/StepPhase.java
+++ src/main/java/io/cosmosoftware/kite/steps/StepPhase.java
@@ -1,7 +1,30 @@
 package io.cosmosoftware.kite.steps;
 
 public enum StepPhase {
-  RAMPUP,
-  LOADREACHED,
-  ALL
+  DEFAULT(""),
+  RAMPUP("Ramp Up "),
+  LOADREACHED("Load Reached "),
+  ALL("All");
+
+  private final String name;
+
+  private StepPhase(String value) {
+    name = value;
+  }
+  
+  public String getName() {
+    return this.name;
+  }
+
+  public String getShortName() {
+    switch (this) {
+      case RAMPUP:
+        return "RU ";
+      case LOADREACHED:
+        return "LR ";
+      default:
+        return "";
+    }
+  }
+  
 }
