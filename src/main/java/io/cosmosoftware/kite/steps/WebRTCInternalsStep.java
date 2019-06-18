afa291203ce21aec863080ed2ba5c34b289d0185
ManuCosmo <emmanuel.andre@cosmosoftware.io>
2019-06-18 17:45:28 +0800

    Added WebRTCInternalsStep

---
+++ src/main/java/io/cosmosoftware/kite/steps/WebRTCInternalsStep.java
@@ -0,0 +1,30 @@
+package io.cosmosoftware.kite.steps;
+
+import io.cosmosoftware.kite.exception.KiteTestException;
+import io.cosmosoftware.kite.pages.WebRTCInternalPage;
+import io.cosmosoftware.kite.report.Reporter;
+
+public class WebRTCInternalsStep extends TestStep {
+
+  private final WebRTCInternalPage webRTCInternalPage;
+
+  public WebRTCInternalsStep(StepParams params) {
+    super(params);
+    setStepPhase(StepPhase.ALL);
+    webRTCInternalPage = new WebRTCInternalPage(params);
+  }
+
+  @Override
+  protected void step() throws KiteTestException {
+    Reporter.getInstance().textAttachment(report, "webrtc-internals dump",
+      webRTCInternalPage.downloadDump(), "plain");
+  }
+
+  @Override
+  public String stepDescription() {
+    return "Gets the webrtc-internals dump.";
+  }
+
+
+
+}
