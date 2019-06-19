1abb7e4f3d2a6c697bcf16623e9ce60e09836d6c
ManuCosmo <emmanuel.andre@cosmosoftware.io>
2019-06-19 14:59:00 +0800

    catch any exception and re-throw as KiteInteractionException

--- src/main/java/io/cosmosoftware/kite/pages/WebRTCInternalPage.java
+++ src/main/java/io/cosmosoftware/kite/pages/WebRTCInternalPage.java
@@ -2,6 +2,7 @@ package io.cosmosoftware.kite.pages;
 
 import io.cosmosoftware.kite.exception.KiteInteractionException;
 import io.cosmosoftware.kite.interfaces.Runner;
+import io.cosmosoftware.kite.report.Status;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -35,25 +36,28 @@ public class WebRTCInternalPage extends BasePage {
     if (!isChrome(webDriver)) {
       return "webrtc-internals is only supported on Chrome";
     }
-    ((JavascriptExecutor) webDriver).executeScript("window.open()");
-    ArrayList<String> tabs = new ArrayList<>(webDriver.getWindowHandles());
-    webDriver.switchTo().window(tabs.get(1));
-    webDriver.get("chrome://webrtc-internals/");
-    waitAround(1500);
-    waitUntilVisibilityOf(summary, 6);
-    click(summary, true);
-    waitUntilVisibilityOf(downloadButton, 6);
-    click(downloadButton);
-    String script = "var dump_object = {" +
-      "  'getUserMedia': userMediaRequests, " +
-      "  'PeerConnections': peerConnectionDataStore, " +
-      "  'UserAgent': navigator.userAgent " +
-      "}; " +
-      "return JSON.stringify(dump_object, null, ' '); ";
-    String internalDump = (String)((JavascriptExecutor) webDriver).executeScript(script);
-    webDriver.switchTo().window(tabs.get(0));
-    return internalDump;
-    
+    try {
+      ((JavascriptExecutor) webDriver).executeScript("window.open()");
+      ArrayList<String> tabs = new ArrayList<>(webDriver.getWindowHandles());
+      webDriver.switchTo().window(tabs.get(1));
+      webDriver.get("chrome://webrtc-internals/");
+      waitAround(1500);
+      waitUntilVisibilityOf(summary, 6);
+      click(summary, true);
+      waitUntilVisibilityOf(downloadButton, 6);
+      click(downloadButton);
+      String script = "var dump_object = {" +
+        "  'getUserMedia': userMediaRequests, " +
+        "  'PeerConnections': peerConnectionDataStore, " +
+        "  'UserAgent': navigator.userAgent " +
+        "}; " +
+        "return JSON.stringify(dump_object, null, ' '); ";
+      String internalDump = (String)((JavascriptExecutor) webDriver).executeScript(script);
+      webDriver.switchTo().window(tabs.get(0));
+      return internalDump;
+    } catch (Exception e) {
+      throw new KiteInteractionException("Unable to get webrtc-internals", e);
+    }    
   }
     
   
