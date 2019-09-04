c17f056441bdaa376cd1e543abd18c7145f6edf0
ManuCosmo <emmanuel.andre@cosmosoftware.io>
2019-09-04 18:42:32 +0800

    Enable ResizeWindowStep to also set the position

--- src/main/java/io/cosmosoftware/kite/steps/ResizeWindowStep.java
+++ src/main/java/io/cosmosoftware/kite/steps/ResizeWindowStep.java
@@ -7,16 +7,29 @@ package io.cosmosoftware.kite.steps;
 import io.cosmosoftware.kite.exception.KiteTestException;
 import io.cosmosoftware.kite.interfaces.Runner;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 
 public class ResizeWindowStep extends TestStep {
-  
+
   protected final int width;
   protected final int height;
+  protected final int x;
+  protected final int y;
 
   public ResizeWindowStep(Runner runner, int width, int height) {
     super(runner);
     this.width = width;
     this.height = height;
+    this.x = -1;
+    this.y = -1;
+  }
+
+  public ResizeWindowStep(Runner runner, int width, int height, int x, int y) {
+    super(runner);
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
   }
 
   @Override
@@ -26,7 +39,12 @@ public class ResizeWindowStep extends TestStep {
 
   @Override
   protected void step() throws KiteTestException {
-    Dimension d = new Dimension(width,height);
+    Dimension currentSize = webDriver.manage().window().getSize();
+    Dimension d = new Dimension(width, height);
     webDriver.manage().window().setSize(d);
+    if (x != -1 && y != -1) {
+      Point p = new Point(Math.min(x, currentSize.width - width), Math.min(y, currentSize.height - height));
+      webDriver.manage().window().setPosition(p);
+    }
   }
 }
