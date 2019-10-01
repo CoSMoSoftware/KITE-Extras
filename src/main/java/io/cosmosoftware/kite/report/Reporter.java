edede443c3f1a0f8824044ce595efb968ccff5d9
namvuCosmo <hoang.vu@cosmosoftware.io>
2019-10-01 12:09:44 +0800

    added new function to reporter

--- src/main/java/io/cosmosoftware/kite/report/Reporter.java
+++ src/main/java/io/cosmosoftware/kite/report/Reporter.java
@@ -118,12 +118,11 @@ public class Reporter {
         logger.warn("Adding " + test.getTestClientMatrix() + " to retry list..");
       }
     }
-    generateFile("environment.properties", this.environment.toString());
-    generateFile("categories.json", generateCategories());
+    generateFile(this.reportPath + "environment.properties", this.environment.toString());
+    generateFile(this.reportPath + "categories.json", generateCategories());
     if (generateRetryConfigFile() != null) {
       logger.warn("Done! Some test cases might need to be rerun!");
-      generateFile(retryPath + this.testConfig.getString("name")
-              .replaceAll(" ", "_").replaceAll(":","_") + "-" + timestamp,
+      generateFile(this.retryPath + this.testName + timestamp() + ".json",
           generateRetryConfigFile());
     } else {
       logger.warn("Done! All test cases have passed!");
@@ -378,27 +377,28 @@ public class Reporter {
   }
 
   private synchronized void generateFile(String fileName, String content) {
-    File file = new File(this.reportPath + fileName);
-    if (!file.exists()) {
-      BufferedWriter writer = null;
-      try {
-        // Writes bytes from the specified byte array to this file output stream
-        writer = new BufferedWriter(new FileWriter(file));
-        writer.write(content);
+    File file = new File( fileName);
+    if (file.exists()) {
+      file.delete();
+    }
+    BufferedWriter writer = null;
+    try {
+      // Writes bytes from the specified byte array to this file output stream
+      writer = new BufferedWriter(new FileWriter(file));
+      writer.write(content);
 
-      } catch (FileNotFoundException e) {
-        logger.error("File not found" + e);
-      } catch (IOException ioe) {
-        logger.error("Exception while writing file " + ioe);
-      } finally {
-        // close the streams using close method
-        try {
-          if (writer != null) {
-            writer.close();
-          }
-        } catch (IOException ioe) {
-          logger.error("Error while closing stream: " + ioe);
+    } catch (FileNotFoundException e) {
+      logger.error("File not found" + e);
+    } catch (IOException ioe) {
+      logger.error("Exception while writing file " + ioe);
+    } finally {
+      // close the streams using close method
+      try {
+        if (writer != null) {
+          writer.close();
         }
+      } catch (IOException ioe) {
+        logger.error("Error while closing stream: " + ioe);
       }
     }
   }
@@ -448,5 +448,13 @@ public class Reporter {
 
   public synchronized Environment getEnvironment() {
     return environment;
+  }
+
+  public synchronized int numberOfRegisteredTest() {
+    return this.tests.size();
+  }
+
+  public synchronized void setStartTime(long startTime) {
+    this.startTime = startTime;
   }
 }
